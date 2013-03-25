3c890ecd2689c92ae86e54fcd1448dbd37b2689d
Michiel Staessen <staessenmichiel@gmail.com>
2013-03-25 19:44:03 +0100

    Wizard structure

---
+++ app/Wizard/controller.rb
@@ -0,0 +1,25 @@
+require 'helpers/action_helper'
+
+class WizardController < Rho::RhoController
+    include ActionHelper
+
+    def index
+
+    end
+
+    def info
+
+    end
+
+    def overview
+
+    end
+
+    def add
+
+    end
+
+    def sign
+
+    end
+end
